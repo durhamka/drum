f3e59233bc45911d096ee0474c0407696d7b18ba
Kinsey Ann Durham <kinseyann505@gmail.com>
2017-10-15 13:58:07 -0600

    Merge 3 patterns into one song

--- models/song_attribute_builder.rb
+++ models/song_attribute_builder.rb
@@ -1,8 +1,10 @@
 class SongAttributeBuilder
   def build(sound_types:, patterns:)
-    sound_types.zip(patterns).map do |sound_type, pattern|
+    beat = sound_types.zip(patterns).map do |sound_type, pattern|
       chars = pattern.split('')
-      chars.map { |char| char.gsub('x', "#{sound_type}") }.join(' ')
+      chars.map { |char| char.gsub('x', "#{sound_type}") }
     end
+
+    beat[0].zip(beat[1], beat[2]).flatten
   end
 end
--- spec/song_attribute_builder_spec.rb
+++ spec/song_attribute_builder_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe SongAttributeBuilder do
   describe '#build' do
     it 'builds the song attributes with the patterns and sound_types' do
       sound_types = %w(kick hitthat snare)
-        patterns = %w(xxxx.... ....xxxx x.x.x.x.)
-      expected_result = ['kick kick kick kick . . . .', '. . . . hitthat hitthat hitthat hitthat', 'snare . snare . snare . snare .']
+      patterns = %w(xxxx.... ....xxxx x.x.x.x.)
 
+      expected_result = %w(kick . snare kick . . kick . snare kick . . . hitthat snare . hitthat . . hitthat snare . hitthat .)
       expect(subject.build(sound_types: sound_types, patterns: patterns)).to eq expected_result
     end
   end
